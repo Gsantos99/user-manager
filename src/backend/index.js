97e28971e82fa62966351f81b251f833b99795b6
Giulyano Santos <giulyanofelipesantos@gmail.com>
2024-09-11 11:29:54 -0300

    finalizando ajustes inicias de arquivo de index

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,32 +1,32 @@
-import express from 'express'
-import cors from 'cors'
-import dotenv from 'dotenv'
-import db from './config/db.js'
-import mainRouter from './routes/router.js'
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import pool from "./config/db.js";
+import mainRouter from "./routes/router.js";
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
-const port = process.env.APP_PORT || 8000
+const app = express();
+const port = process.env.APP_PORT || 8000;
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-app.use('/api', mainRouter)
+app.use(mainRouter);
 
 // Test the connection to the database
-db.connect(err => {
+pool.connect((err) => {
   if (err) {
-    console.error('Error connecting to PostgreSQL', err)
+    console.error("Error connecting to PostgreSQL", err);
   } else {
-    console.log('Connected to PostgreSQL')
+    console.log("Connected to PostgreSQL");
   }
-})
+});
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`)
-})
+  console.log(`Server is running on port ${port}`);
+});
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
 });
