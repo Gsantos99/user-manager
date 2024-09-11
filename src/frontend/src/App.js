cd6329b8f7fc13bb346bb5f60a22cf4a9aa0584b
Giulyano Santos <giulyanofelipesantos@gmail.com>
2024-09-11 12:51:38 -0300

    Adicionando o toastify no app e o global style

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,8 +1,13 @@
+import GlobalStyle from "./styles/global";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 function App() {
   return (
-    <div>
-      <h2>Hello World!</h2>
-    </div>
+    <>
+      <ToastContainer autoClose={3000} position={toast.POSITION.BOTTOM_LEFT} />
+      <GlobalStyle />
+    </>
   );
 }
 
