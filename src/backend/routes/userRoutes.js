49d7e3c64017e57ae1b816e27a929b9bfa649135
Giulyano Santos <giulyanofelipesantos@gmail.com>
2024-09-11 14:19:55 -0300

    adicionando as rotas da api de users

--- src/backend/routes/userRoutes.js
+++ src/backend/routes/userRoutes.js
@@ -4,5 +4,10 @@ import * as userController from "../controllers/userController.js"
 const router = express.Router()
 
 router.get("/allUsers", userController.getAllUsers)
+router.post('/', userController.createUser);
+router.get('/:id', userController.getUserById);
+router.put('/:id', userController.updateUser);
+router.delete('/:id', userController.deleteUser);
+
 
 export default router
