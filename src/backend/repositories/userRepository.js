fcf8d84ff3450310531eb46a1e755283b207858e
Giulyano Santos <giulyanofelipesantos@gmail.com>
2024-09-11 12:07:03 -0300

    Finalizando crud de user na camada repository

--- src/backend/repositories/userRepository.js
+++ src/backend/repositories/userRepository.js
@@ -4,3 +4,30 @@ export const getAllUsers = async () => {
   const result = await pool.query("SELECT * FROM users");
   return result.rows;
 };
+
+export const createUser = async (name, email, phone, date_of_birth) => {
+  const result = await pool.query(
+    "INSERT INTO users (name, email, phone, date_of_birth) VALUES ($1, $2, $3, $4) RETURNING *",
+    [name, email, phone, date_of_birth]
+  );
+  return result.rows[0];
+};
+
+export const getUserById = async (id) => {
+  const result = await pool.query("SELECT * FROM users WHERE id = $1", [id]);
+  return result.rows[0];
+};
+
+export const updateUser = async (id, name, email, phone, date_of_birth) => {
+  await pool.query("UPDATE users SET name = $1, email = $2, phone = $3, date_of_birth = $4 WHERE id = $3", [
+    name,
+    email,
+    phone,
+    date_of_birth,
+    id,
+  ]);
+};
+
+export const deleteUser = async (id) => {
+  await pool.query("DELETE FROM users WHERE id = $1", [id]);
+};
