aed6900de5317be4da8a36e7398cecdf07acf3e2
Giulyano Santos <giulyanofelipesantos@gmail.com>
2024-09-11 13:18:55 -0300

    Organizando o app

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,11 +1,41 @@
-import GlobalStyle from "./styles/global";
+// React
+import { useEffect, useState } from "react";
+import axios from "axios";
 import { toast, ToastContainer } from "react-toastify";
+// Styles
+import GlobalStyle from "./styles/global";
 import "react-toastify/dist/ReactToastify.css";
+// Components
+import Form from "./components/Form.js";
+import Grid from "./components/Grid";
+import { Container } from "./components/Container";
+import { Title } from "./components/Title";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [onEdit, setOnEdit] = useState(null);
+
+  const getUsers = async () => {
+    try {
+      const res = await axios.get("http://localhost:8800");
+      setUsers(res.data.sort((a, b) => (a.nome > b.nome ? 1 : -1)));
+    } catch (error) {
+      toast.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, [setUsers]);
+
   return (
     <>
-      <ToastContainer autoClose={3000} position={toast.POSITION.BOTTOM_LEFT} />
+      <Container>
+        <Title>Gerenciador de Usuários </Title>
+        <Form onEdit={onEdit} setOnEdit={setOnEdit} getUsers={getUsers} />
+        <Grid setOnEdit={setOnEdit} users={users} setUsers={setUsers} />
+      </Container>
+      <ToastContainer autoClose={3000} position={toast?.POSITION?.BOTTOM_LEFT} />
       <GlobalStyle />
     </>
   );
