bb6397c079cf7e100d60b3ad1e62fe7053e47061
Giulyano Santos <giulyanofelipesantos@gmail.com>
2024-09-11 10:31:37 -0300

    Adicionando teste de conexão com o bando de dados ao iniciar aplicação

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,13 +1,29 @@
 import express from 'express'
 import cors from 'cors'
-import dotenv from 'dotenv';
+import dotenv from 'dotenv'
+import pool from './config/db.js'
+
+dotenv.config()
 
 const app = express()
-const port = 8080
+const port = process.env.APP_PORT || 8000
 
 app.use(express.json())
 app.use(cors())
 
-app.listen(port, () => {
-  console.log('Server is running on port 8080')
+// Test the connection to the database
+pool.connect(err => {
+  if (err) {
+    console.error('Error connecting to PostgreSQL', err)
+  } else {
+    console.log('Connected to PostgreSQL')
+  }
 })
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+})
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
